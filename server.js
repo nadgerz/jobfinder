59d39828671f494561553946567b2c5b4af26aa8
Stephen Ingram <k.mobbit@gmail.com>
2014-09-23 19:15:24 +0000

    mongodb instead of hard code

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var express = require("express");
+var mongoose= require("mongoose");
 
 var app = express();
 
@@ -7,8 +8,20 @@ app.set('view engine', 'jade');
 
 app.use(express.static(__dirname + '/public'));
 
+app.get('/api/jobs', function(req, res) {
+    res.send('test');
+});
+
 app.get('*', function(req, res) {
     res.render('index');
+});
+
+mongoose.connect('mongodb://localhost/jobfinder');
+
+var conn = mongoose.connection;
+
+conn.once('open', function() {
+    console.log('connected to mongo successfully');
 });
 
 // we need to do the following rather than just a port number because
