c18a4f9c736b6c38cbe849579a6eb51d70538896
Stephen Ingram <k.mobbit@gmail.com>
2014-09-24 11:55:18 +0000

    no now we wrapp the connect around the find, where the find is the callback to the connect

--- test/jobs-data-spec.js
+++ test/jobs-data-spec.js
@@ -2,13 +2,14 @@ var expect = require("chai").expect;
 var mongoose = require("mongoose");
 var jobModel = require("../models/Job");
 
-mongoose.connect('mongodb://localhost/jobfinder');
 
 describe("get jobs", function() {
     it("should never be empty since jobs are seeded", function(done) {
-        mongoose.model('Job').find({}).exec(function(err, jobsList) {
-            expect(jobsList.length).to.be.at.least(1);
-            done();
+        mongoose.connect('mongodb://localhost/jobfinder', function() {
+            mongoose.model('Job').find({}).exec(function(err, jobsList) {
+                expect(jobsList.length).to.be.at.least(1);
+                done();
+            });
         });
     });
 });
