c42679eccd95ddc9aa54eb2bb70302029f1b005d
Stephen Ingram <k.mobbit@gmail.com>
2014-09-24 13:54:13 +0000

    we will take one of our functions and convert it so that it works with bluebird and returns a promise.
    So, resetJobs will return a new promise.
    
    And that takes a callback with a resolve and reject.

--- test/jobs-data-spec.js
+++ test/jobs-data-spec.js
@@ -1,12 +1,14 @@
 var expect = require("chai").expect;
 var mongoose = require("mongoose");
 var jobModel = require("../models/Job");
+var Promise = require("bluebird");
 
 
 function resetJobs(callback) {
-    mongoose.connection.collections['jobs'].drop(callback);
+    return new Promise( function(resolve, reject) {
+        mongoose.connection.collections['jobs'].drop(callback);
+    });
 }
-
 
 describe("get jobs", function() {
     it("should never be empty since jobs are seeded", function(done) {
