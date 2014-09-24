de0454b4228234cee850bd67b4f226721b29d35a
Stephen Ingram <k.mobbit@gmail.com>
2014-09-24 14:15:51 +0000

    Now we move on to seedJobs.
    We add a bluebird reference...
    
    And wrap our functionality with another promise.
    
    We can remove the callback param to seedJobs and replace the callback call, with resolve.

--- models/Job.js
+++ models/Job.js
@@ -10,14 +10,16 @@ var Job = mongoose.model('Job', jobSchema);
 
 exports.seedJobs = function() {
     return new Promise( function(resolve, reject) {
-        Job.find({}).exec(function(err, collection) {
+        Job.find({}).exec(function(error, collection) {
+            // console.log("error");
+            // console.log(collection.length);
             if (collection.length === 0) {
                 Job.create({title:'xxx', description: 'flibble'});
                 Job.create({title:'abc', description: 'glibble'});
                 Job.create({title:'123', description: 'hlibble'});
                 Job.create({title:'bob', description: 'jlibble'}, resolve);
-            };
+            }
         });
     });
-}
+};
 
