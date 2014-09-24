dda7f76ccb270f95fd56c98d248b7120b54f2b4c
Stephen Ingram <k.mobbit@gmail.com>
2014-09-24 12:00:23 +0000

    we need to test our seed functionality because as it stands our test was checking a seeded database, so we reset the db on each test and this will test if the db actually gets seeded

--- test/jobs-data-spec.js
+++ test/jobs-data-spec.js
@@ -3,12 +3,19 @@ var mongoose = require("mongoose");
 var jobModel = require("../models/Job");
 
 
+function resetJobs(callback) {
+    mongoose.connect.collections['jobs'].drop(callback);
+}
+
+
 describe("get jobs", function() {
     it("should never be empty since jobs are seeded", function(done) {
         mongoose.connect('mongodb://localhost/jobfinder', function() {
-            mongoose.model('Job').find({}).exec(function(err, jobsList) {
-                expect(jobsList.length).to.be.at.least(1);
-                done();
+            resetJobs(function() {
+                mongoose.model('Job').find({}).exec(function(err, jobsList) {
+                    expect(jobsList.length).to.be.at.least(1);
+                    done();
+                });
             });
         });
     });
