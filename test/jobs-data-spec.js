81b1ed829bbdb98423a01b9858d9dad80666f984
Stephen Ingram <k.mobbit@gmail.com>
2014-09-24 14:49:37 +0000

    Now we can promisify our job getter calls, but there are a lot of params, so we need to use a thing called Promise.cast We move the mongoose query into the function and then we can call the findJobs in the test And we can get rid of the error as we are not handling it any more and the jobsList is the first thing to come in
    And finally get rid of the anonymous function
    Hmm... not sure why we have to remove the query... I know we will fix it later, but for now the empty object will work like this

--- test/jobs-data-spec.js
+++ test/jobs-data-spec.js
@@ -12,16 +12,19 @@ function resetJobs() {
 
 var connectDB = Promise.promisify(mongoose.connect, mongoose);
 
+function findJobs(query) {
+    return Promise.cast(mongoose.model('Job').find(query).exec());
+}
+
 describe("get jobs", function() {
     it("should never be empty since jobs are seeded", function(done) {
         connectDB('mongodb://localhost/jobfinder')
             .then(resetJobs)
             .then(jobModel.seedJobs)
-            .then(function() {
-                mongoose.model('Job').find({}).exec(function(err, jobsList) {
-                    expect(jobsList.length).to.be.at.least(1);
-                    done();
-                });
+            .then(findJobs)
+            .then(function(jobsList) {
+                expect(jobsList.length).to.be.at.least(1);
+                done();
             });
     });
 });
