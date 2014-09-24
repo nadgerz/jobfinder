68b2867243be5b490b4eeb0361e81f5f5ef337bd
Stephen Ingram <k.mobbit@gmail.com>
2014-09-24 11:51:59 +0000

    so mocha needs to indicae when a test is done, which if we do not call, defaults to a timeout of 2 seconds

--- test/jobs-data-spec.js
+++ test/jobs-data-spec.js
@@ -5,7 +5,7 @@ var jobModel = require("../models/Job");
 mongoose.connect('mongodb://localhost/jobfinder');
 
 describe("get jobs", function() {
-    it("should never be empty since jobs are seeded", function() {
+    it("should never be empty since jobs are seeded", function(done) {
         mongoose.model('Job').find({}).exec(function(err, jobsList) {
             expect(jobsList.length).to.be.at.least(1);
         });
