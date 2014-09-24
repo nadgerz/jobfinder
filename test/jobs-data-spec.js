b9261279471dd9d5f91dd64bd39b9fca9aef4cda
Stephen Ingram <k.mobbit@gmail.com>
2014-09-24 13:59:36 +0000

    Instead of calling our callback when it is successful, wewill call resolve, and if it fails or there is an error, we will call reject.
    So we no longer need our callback.
    
    Now, where we had our callback in the returnJobs call... we add a 'then' call.
    
    This ultimately allows us to chain calls, and, I suspect, full the functions out.

--- test/jobs-data-spec.js
+++ test/jobs-data-spec.js
@@ -4,16 +4,16 @@ var jobModel = require("../models/Job");
 var Promise = require("bluebird");
 
 
-function resetJobs(callback) {
+function resetJobs() {
     return new Promise( function(resolve, reject) {
-        mongoose.connection.collections['jobs'].drop(callback);
+        mongoose.connection.collections['jobs'].drop(resolve, reject);
     });
 }
 
 describe("get jobs", function() {
     it("should never be empty since jobs are seeded", function(done) {
         mongoose.connect('mongodb://localhost/jobfinder', function() {
-            resetJobs(function() {
+            resetJobs().then(function() {
                 jobModel.seedJobs(function() {
                     mongoose.model('Job').find({}).exec(function(err, jobsList) {
                         expect(jobsList.length).to.be.at.least(1);
