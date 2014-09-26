96c593cb41b18ef644bd837a80aa52e2994b7745
Stephen Ingram <k.mobbit@gmail.com>
2014-09-26 15:23:39 +0000

    play catchup with what I did in satoyma dir

--- test/jobs-data-spec.js
+++ test/jobs-data-spec.js
@@ -1,30 +1,44 @@
-var expect = require("chai").expect;
-var mongoose = require("mongoose");
-var jobModel = require("../models/Job");
-var Promise = require("bluebird");
-
+ar expect = require("chai").expect;
+var mongoose = require('mongoose');
+var jobModel = require('../models/Job');
+var Promise = require('bluebird');
+var jobsData = require('../jobs-data.js');
 
 function resetJobs() {
     return new Promise( function(resolve, reject) {
         mongoose.connection.collections['jobs'].drop(resolve, reject);
     });
-}
+};
 
-var connectDB = Promise.promisify(mongoose.connect, mongoose);
-
-function findJobs(query) {
-    return Promise.cast(mongoose.model('Job').find(query).exec());
-}
 
 describe("get jobs", function() {
-    it("should never be empty since jobs are seeded", function(done) {
-        connectDB('mongodb://localhost/jobfinder')
+
+    var jobs;
+    var db_url;
+    db_url = 'mongodb://dms:nrg@ds039850.mongolab.com:39850/jobfinder';
+    db_url = 'mongodb://localhost/jobfinder';
+
+    before(function(done) {
+        this.timeout(5000);
+        jobsData.connectDB(db_url)
             .then(resetJobs)
             .then(jobModel.seedJobs)
-            .then(findJobs)
-            .then(function(jobsList) {
-                expect(jobsList.length).to.be.at.least(1);
+            .then(jobsData.findJobs)
+            .then(function(collection) {
+                jobs = collection;
                 done();
             });
+    });
+
+    it("should never be empty since jobs are seeded", function() {
+        expect(jobs.length).to.be.at.least(1);
+    });
+
+    it("should have a job with a title", function() {
+        expect(jobs[0].title).to.not.be.empty;
+    });
+
+    it("should have a job with a description", function() {
+        expect(jobs[0].description).to.not.be.empty;
     });
 });
