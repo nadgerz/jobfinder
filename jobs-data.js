7c56c1d79a52b76f0024d1bcad8a06e91bbe6ae2
Stephen Ingram <k.mobbit@gmail.com>
2014-09-26 16:22:55 +0000

    trying to get to the same point as alex zanfir

--- jobs-data.js
+++ jobs-data.js
@@ -1,8 +1,34 @@
 var mongoose = require('mongoose');
 var Promise = require('bluebird');
 
-exports.findJobs = function (query) {
-    return Promise.cast(mongoose.model('Job').find(query).exec());
+var Job = mongoose.model('Job');
+
+var findJobs = function (query) {
+    return Promise.cast(Job.find(query).exec());
 }
 
+exports.findJobs = findJobs;
+
+
 exports.connectDB = Promise.promisify(mongoose.connect, mongoose);
+
+
+var createJob = Promise.promisify(Job.create, Job);
+
+exports.seedJobs = function() {
+    return findJobs({}).then(function(collection) {
+        if (collection.length === 0) {
+            return Promise.map(jobs, function(job) {
+                return createJob(job);
+            });
+        }
+    });
+};
+
+var jobs = [
+        {title:'xxx', description: 'flibble'},
+        {title:'abc', description: 'glibble'},
+        {title:'123', description: 'hlibble'},
+        {title:'bob', description: 'jlibble'},
+    ];
+    
--- models/Job.js
+++ models/Job.js
@@ -1,34 +1,8 @@
 var mongoose = require("mongoose");
-var Promise = require("bluebird");
 
 var jobSchema = mongoose.Schema({
     title:       {type:String},
     description: {type:String}
 });
 
-var jobs = [
-        {title:'xxx', description: 'flibble'},
-        {title:'abc', description: 'glibble'},
-        {title:'123', description: 'hlibble'},
-        {title:'bob', description: 'jlibble'},
-    ];
-    
-    
-var Job = mongoose.model('Job', jobSchema);
-
-function findJobs(query) {
-    return Promise.cast(mongoose.model('Job').find(query).exec());
-}
-
-var createJob = Promise.promisify(Job.create, Job);
-
-exports.seedJobs = function() {
-    return findJobs({}).then(function(collection) {
-        if (collection.length === 0) {
-            return Promise.map(jobs, function(job) {
-                return createJob(job);
-            });
-        }
-    });
-};
-
+mongoose.model('Job', jobSchema);
